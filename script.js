933237580c76ca11e2f77fc86d61952e65927b19
Deviee <80790777+D3eviee@users.noreply.github.com>
2024-08-26 17:40:37 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function closeSidebar(){
+    document.getElementById("navigation_menu-side-menu").style.display = "none";
+}
+
+function showSidebar(){
+    document.getElementById("navigation_menu-side-menu").style.display = "block";
+}
